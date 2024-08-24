84cbe5ab11c039128883ad2eb3db0ae4198ce360
Juan <juansjimenez1@gmail.com>
2024-08-24 15:04:41 -0300

    Add middleware

--- src/config/mongo-connection.ts
+++ src/config/mongo-connection.ts
@@ -1,28 +1,23 @@
 const mongoUri = (serviceName: string, clusterName: string): string => `mongodb+srv://${encodeURIComponent(process.env.AWS_ACCESS_KEY_ID ?? '')}:${encodeURIComponent(process.env.AWS_SECRET_ACCESS_KEY ?? '')}@${encodeURIComponent(clusterName)}.7ajmx.mongodb.net/${encodeURIComponent(serviceName)}?authSource=%24external&authMechanism=MONGODB-AWS&retryWrites=true&w=majority&authMechanismProperties=AWS_SESSION_TOKEN:${encodeURIComponent(process.env.AWS_SESSION_TOKEN ?? '')}`;
 
-const localMongoUri = 'mongodb://localhost:27017/test';
-
 const clusterName = (stage: string): string => {
   switch (stage) {
     case 'dev':
-      return 'andromeda-dev';
+      return 'andromeda';
     default:
       throw new Error('There was a problem connecting to the database, review the selected stage');
   }
 };
 
-const mongoConnectionString = ({
+const mongoConnection = ({
   serviceName,
   stage,
 }: {
   serviceName: string,
   stage: string,
 }): string => {
-  if (stage === 'local') {
-    return localMongoUri;
-  }
   const cluster = clusterName(stage);
   return mongoUri(serviceName, cluster);
 };
 
-export default mongoConnectionString;
+export default mongoConnection;
--- src/services/users/handlers/get-or-create/handler.ts
+++ src/services/users/handlers/get-or-create/handler.ts
@@ -1,15 +1,23 @@
 import { APIGatewayProxyEvent } from 'aws-lambda';
 import middy from '@middy/core';
 import { StatusCodes } from 'http-status-codes';
+import httpErrorHandler from '@middy/http-error-handler';
+import doNotWaitForEmptyEventLoop from '@middy/do-not-wait-for-empty-event-loop';
 import config from '../../config';
 import addGetCors from '../../../../commons/middlewares/cors/add-get-cors';
 import injectDatabaseConnection from '../../../../commons/middlewares/injections/inject-database-connection';
 import HandlerResponse from '../../../../types/handler-response';
 import { getOrCreate } from '../../actions/get-or-create';
+import jsonTextPlainHttpResponseSerializer from '../../../../commons/middlewares/custom/json-text-plain-http-response-serializer';
+import errorLogger from '../../../../commons/middlewares/custom/error-logger';
 
 const middlewares = [
   addGetCors(),
+  doNotWaitForEmptyEventLoop(),
   injectDatabaseConnection({ uri: config.mongoUri }),
+  jsonTextPlainHttpResponseSerializer(),
+  httpErrorHandler(),
+  errorLogger(),
 ];
 
 export const main = middy(async (
