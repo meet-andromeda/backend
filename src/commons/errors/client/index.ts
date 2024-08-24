f49b2253a9239b7d7f11b0253bf999d6c424b075
Juan <juansjimenez1@gmail.com>
2024-08-23 23:45:04 -0300

    Delete unnecessary test files

--- src/commons/errors/client/index.ts
+++ src/commons/errors/client/index.ts
@@ -2,12 +2,10 @@ import createBadRequestError from './create-bad-request-error';
 import createForbiddenError from './create-forbidden-error';
 import createNotFoundError from './create-not-found-error';
 import createUnauthorizedError from './create-unauthorized-error';
-import isClientError from './is-client-error';
 
 export {
   createBadRequestError,
   createForbiddenError,
   createNotFoundError,
   createUnauthorizedError,
-  isClientError,
 };
--- src/commons/errors/server/index.ts
+++ src/commons/errors/server/index.ts
@@ -2,12 +2,10 @@ import createBadGatewayError from './create-bad-gateway-error';
 import createInternalServerError from './create-internal-server-error';
 import createGatewayTimeoutError from './create-gateway-timeout-error';
 import createServiceUnavailableTimeoutError from './create-service-unavailable-error';
-import isServerError from './is-server-error';
 
 export {
   createBadGatewayError,
   createInternalServerError,
   createGatewayTimeoutError,
   createServiceUnavailableTimeoutError,
-  isServerError,
 };
