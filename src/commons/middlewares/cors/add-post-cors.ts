419e31d069adca1f39daafe11831ff7b8f398270
Diego L <diegolarrainy@gmail.com>
2024-08-23 23:50:04 -0300

    :broom: fix linter

--- src/commons/middlewares/cors/add-post-cors.ts
+++ src/commons/middlewares/cors/add-post-cors.ts
@@ -2,7 +2,7 @@ import cors from '@middy/http-cors';
 
 /**
  * Add POST CORS headers to the response
- * 
+ *
  * @returns middy.Middleware.Obj
  */
 function addPostCors(): ReturnType<typeof cors> {
